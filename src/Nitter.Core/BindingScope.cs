fd0cbad7bdbb6084e57660fd8ee4cf394ddd937c
Mehmet YILDIZ <Mehmet.Yildiz-ext@bshg.com>
2019-05-15 19:28:55 +0300

    Nitter.Core initial types introduced

---
+++ src/Nitter.Core/BindingScope.cs
@@ -0,0 +1,14 @@
+﻿namespace Nitter.Core
+{
+    /// <summary> Scope for the auto binding of dependencies. </summary>
+    public enum BindingScope
+    {
+        /// <summary>A new instance will be created every time it is requested.</summary>
+        Transient,
+
+        /// <summary>Only one instance will exists in the application domain,
+        /// and it will be returned every time it is requested.
+        /// The Singleton type must be thread-safe.</summary>
+        Singleton
+    }
+}
